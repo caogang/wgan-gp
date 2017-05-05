Fix : calc gradient penalty
Find bug in pytorch, and push an issue.

gan_toy.py
@@ -182,23 +182,21 @@ def calc_gradient_penalty(netD, real_data, fake_data):
     alpha = torch.rand(BATCH_SIZE, 1)
     alpha = alpha.expand(real_data.size())
     alpha = alpha.cuda() if use_cuda else alpha
-    print alpha.size(), real_data.size(), fake_data.size()
+
     interpolates = alpha * real_data + ((1 - alpha) * fake_data)
     if use_cuda:
         interpolates = interpolates.cuda()
     interpolates = autograd.Variable(interpolates, requires_grad=True)
 
     disc_interpolates = netD(interpolates)
-    print disc_interpolates.size()
+
     gradients = autograd.grad(outputs=disc_interpolates, inputs=interpolates,
                               grad_outputs=torch.ones(BATCH_SIZE).cuda() if use_cuda else torch.ones(BATCH_SIZE),
-                              create_graph=True, only_inputs=True)
-    print gradients
-    for p in netD.parameters(): # reset requires_grad
-            print p.requires_grad
-    autograd.grad(outputs=gradients, inputs=[],
-                          grad_outputs=torch.ones(gradients[0].size()).cuda() if use_cuda else torch.ones(gradients[0].size()),
-                          only_inputs=False)
+                              create_graph=True, only_inputs=True)[0]
+
+    # autograd.grad(outputs=gradients, inputs=[],
+    #                       grad_outputs=torch.ones(gradients[0].size()).cuda() if use_cuda else torch.ones(gradients[0].size()),
+    #                       only_inputs=False)
     #gradients.mean().backward()
     gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean() * LAMBDA
     return gradient_penalty