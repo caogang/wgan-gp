e38185022358c92a34347e224026641edfa3f1eb
Marvin Cao <caogang440@163.com>
2017-06-22 23:33:44 +0800

    Fix : the dimension problem of conv2d

--- gan_mnist.py
+++ gan_mnist.py
@@ -79,13 +79,13 @@ class Discriminator(nn.Module):
         super(Discriminator, self).__init__()
 
         main = nn.Sequential(
-            nn.Conv2D(OUTPUT_DIM, DIM, 5, stride=2),
+            nn.Conv2d(1, DIM, 5, stride=2, padding=2),
             # nn.Linear(OUTPUT_DIM, 4*4*4*DIM),
             nn.LeakyReLU(True),
-            nn.Conv2D(DIM, 2*DIM, 5, stride=2),
+            nn.Conv2d(DIM, 2*DIM, 5, stride=2, padding=2),
             # nn.Linear(4*4*4*DIM, 4*4*4*DIM),
             nn.LeakyReLU(True),
-            nn.Conv2D(2*DIM, 4*DIM, 5, stride=2),
+            nn.Conv2d(2*DIM, 4*DIM, 5, stride=2, padding=2),
             # nn.Linear(4*4*4*DIM, 4*4*4*DIM),
             nn.LeakyReLU(True),
             # nn.Linear(4*4*4*DIM, 4*4*4*DIM),
@@ -94,9 +94,10 @@ class Discriminator(nn.Module):
             # nn.LeakyReLU(True),
         )
         self.main = main
-        self.output = nn.Linear(4*4*4*DIM, 1),
+        self.output = nn.Linear(4*4*4*DIM, 1)
 
     def forward(self, input):
+        input = input.view(-1, 1, 28, 28)
         out = self.main(input)
         out = out.view(-1, 4*4*4*DIM)
         out = self.output(out)
