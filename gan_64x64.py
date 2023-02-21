a20d11cb3e16e5820e787fceb11fd096c13a218b
yoonhero <yoonhero06@naver.com>
2023-02-21 11:05:10 +0900

    wgan 64x64

--- gan_64x64.py
+++ gan_64x64.py
@@ -31,41 +31,31 @@ BATCH_SIZE = 64  # Batch size
 ITERS = 200000  # How many generator iterations to train for
 OUTPUT_DIM = 3*64*64  # Number of pixels in Cat64x64 (3*64*64)
 
+
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
-        preprocess = nn.Sequential(
-            nn.Linear(128, 4 * 4 * 4 * DIM),
+        self.layer = nn.Sequential(
+            nn.ConvTranspose2d(128, 64 * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(64 * 8),
             nn.ReLU(True),
-        )
-
-        block1 = nn.Sequential(
-            nn.ConvTranspose2d(4 * DIM, 2 * DIM, 2, stride=2),
-            nn.BatchNorm2d(2 * DIM),
+            nn.ConvTranspose2d(64 * 8, 64 * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 4),
             nn.ReLU(True),
-        )
-        block2 = nn.Sequential(
-            nn.ConvTranspose2d(2 * DIM, DIM, 2, stride=2),
-            nn.BatchNorm2d(DIM),
+            nn.ConvTranspose2d(64 * 4, 64 * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64 * 2),
             nn.ReLU(True),
+            nn.ConvTranspose2d(64 * 2, 64, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(64, 3, 4, 2, 1, bias=False),
+            nn.Tanh()
         )
-
-        deconv_out = nn.ConvTranspose2d(DIM, 3, 2, stride=2)
-
-        self.preprocess = preprocess
-        self.block1 = block1
-        self.block2 = block2
-        self.deconv_out = deconv_out
-        self.tanh = nn.Tanh()
 
     def forward(self, x: torch.tensor) -> torch.tensor:
-        output = self.preprocess(input)
-        output = output.view(-1, 4 * DIM, 4, 4)
-        output = self.block1(output)
-        output = self.block2(output)
-        output = self.deconv_out(output)
-        output = self.tanh(output)
-        return output.view(-1, 3, 32, 32)
+        output = self.layer(x)
+
+        return output
 
 
 class DiscConvBlock(nn.Module):
@@ -73,10 +63,12 @@ class DiscConvBlock(nn.Module):
         super().__init__()
 
         self.conv_block = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=True, padding_mode="reflect"),
-            nn.LeakyReLU(0.2, inplace=True) if use_activation else nn.Identity()
+            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size,
+                      stride=stride, padding=padding, bias=True, padding_mode="reflect"),
+            nn.LeakyReLU(
+                0.2, inplace=True) if use_activation else nn.Identity()
         )
-    
+
     def forward(self, x: torch.tensor) -> torch.tensor:
         out = self.conv_block(x)
         return out
@@ -97,7 +89,7 @@ class Discriminator(nn.Module):
         out = self.conv2(out)
         out = self.conv3(out)
         out = self.conv4(out)
-        out = self.conv5(out)  
+        out = self.conv5(out)
         out = out.view(out.shape[0], -1)
         return out
 
@@ -148,7 +140,7 @@ def calc_gradient_penalty(netD, real_data, fake_data):
 
 
 def generate_image(frame, netG):
-    fixed_noise_128 = torch.randn(128, 128)
+    fixed_noise_128 = torch.randn(128, 128, 1, 1)
     if use_cuda:
         fixed_noise_128 = fixed_noise_128.cuda(gpu)
 
@@ -167,7 +159,7 @@ def generate_image(frame, netG):
 def get_inception_score(G):
     all_samples = []
     for _ in range(10):
-        samples_100 = torch.randn(100, 128)
+        samples_100 = torch.randn(100, 128, 1, 1)
         if use_cuda:
             samples_100 = samples_100.cuda(gpu)
         samples_100 = autograd.Variable(samples_100, volatile=True)
@@ -223,7 +215,7 @@ for iteration in range(ITERS):
             D_real.backward(mone)
 
             # train with fake
-            noise = torch.randn(BATCH_SIZE, 128)
+            noise = torch.randn(BATCH_SIZE, 128, 1, 1)
             if use_cuda:
                 noise = noise.cuda(gpu)
             noisev = autograd.Variable(
@@ -253,7 +245,7 @@ for iteration in range(ITERS):
                 p.requires_grad = False
             netG.zero_grad()
 
-            noise = torch.randn(BATCH_SIZE, 128)
+            noise = torch.randn(BATCH_SIZE, 128, 1, 1)
             if use_cuda:
                 noise = noise.cuda(gpu)
             noisev = autograd.Variable(noise)
